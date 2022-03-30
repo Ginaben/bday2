366fd6ad2dce9ed99760932557a9e11e0619d2cd
iamgood147@gmail.com <iamgood147@gmail.com>
2022-03-31 01:57:02 +0900

    3.31 01:56 코드 수정3

--- src/main/java/happy/bday2/controller/BDayController.java
+++ src/main/java/happy/bday2/controller/BDayController.java
@@ -57,8 +57,8 @@ public class BDayController {
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
-    //디테일
-    @GetMapping("/detail/{id}")
+    //디테일 (배포시 getmapping url에 / 빼야함..
+    @GetMapping("detail/{id}")
     public String detail(@PathVariable("id") Long id, Model model,
                          RequestPageSortDto requestPageDto){
         model.addAttribute("date",service.getInfo(id));
